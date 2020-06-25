0429ae2ef699a5c1ee533eb80bc6d894b593d2db
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-06-25 11:06:51 +0200

    Added tests for game cell

---
+++ src/game/game-cell.spec.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { GameCell } from './game-cell';
+
+describe('GameCell', () => {
+    it('should match snapshot without highlight', () => {
+        const gameCell = mount(<GameCell x={0} y={0} onClick={jest.fn()} revealedCells={[]} cellsToReveal={[]}/>);
+
+        expect(gameCell).toMatchSnapshot();
+    });
+
+    it('should match snapshot with highlight', () => {
+        const gameCell = mount(<GameCell x={0} y={0} onClick={jest.fn()} revealedCells={[]} cellsToReveal={[{x: 0, y: 0}]}/>);
+
+        expect(gameCell).toMatchSnapshot();
+    });
+
+    it('should display the cell content', () => {
+        const gameCell = mount(<GameCell x={0} y={0} onClick={jest.fn()} revealedCells={[{x: 0, y: 0, content: 'T'}]} cellsToReveal={[]}/>);
+
+        expect(gameCell).toMatchSnapshot();
+    });
+
+    it('should display placeholder if content is absent', () => {
+        const gameCell = mount(<GameCell x={0} y={0} onClick={jest.fn()} revealedCells={[{x: 1, y: 2, content: 'T'}]} cellsToReveal={[]}/>);
+
+        expect(gameCell).toMatchSnapshot();
+    });
+});
